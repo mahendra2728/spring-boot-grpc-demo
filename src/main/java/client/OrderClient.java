fd657481ecfba27e6854a31170c4426589279472
Mahendra M <mahendramaske28@gmail.com>
2022-06-27 10:53:06 +0530

    added order client code and also added inter communication logic

--- src/main/java/client/OrderClient.java
+++ src/main/java/client/OrderClient.java
@@ -1,14 +1,25 @@
 package client;
 
+import com.pm.grpc.orders.Order;
+import com.pm.grpc.orders.OrderRequest;
+import com.pm.grpc.orders.OrderResponse;
 import com.pm.grpc.orders.OrderServiceGrpc;
+import io.grpc.Channel;
 
-import java.nio.channels.Channel;
+import java.util.List;
 
 public class OrderClient  {
 
     private OrderServiceGrpc.OrderServiceBlockingStub orderServiceBlockingStub;
 
     public OrderClient(Channel channel){
+    orderServiceBlockingStub = OrderServiceGrpc.newBlockingStub(channel);
+  }
 
+  public List<Order> getUsersDetails(int userId) {
+    OrderRequest orderRequest = OrderRequest.newBuilder().setUserId(userId).build();
+
+    OrderResponse orderResponse = orderServiceBlockingStub.getOrdersByUserId(orderRequest);
+    return orderResponse.getOrdersList();
     }
 }
--- src/main/java/serviceimpl/UserServiceImpl.java
+++ src/main/java/serviceimpl/UserServiceImpl.java
@@ -1,15 +1,23 @@
 package serviceimpl;
 
+import client.OrderClient;
+import com.pm.grpc.orders.Order;
 import com.pm.grpc.users.UserRequest;
 import com.pm.grpc.users.UserResponse;
 import com.pm.grpc.users.UserServiceGrpc;
 import dto.UserDtoResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
 
     @Override
-    public void getUserDetails(UserRequest request, StreamObserver<UserResponse> responseObserver) {
+    public void getUserDetails(UserRequest request, StreamObserver<UserResponse> responseObserver) throws InterruptedException {
 
         UserDtoResponse dto =new UserDtoResponse();
         dto.setUserId(request.getUserId());
@@ -17,15 +25,42 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
         dto.setUsername("mahi2728");
         dto.setAge(27);
 
-        UserResponse userResponse = UserResponse.newBuilder()
-                .setUserId(dto.getUserId())
-                .setName(dto.getName())
-                .setUsername(dto.getUsername())
-                .setAge(dto.getAge())
-                .build();
+    // Add channel config here to call Order Service
+    ManagedChannel channel =
+        ManagedChannelBuilder.forTarget("localhost:50002").usePlaintext().build();
+
+    OrderClient orderClient = new OrderClient(channel);
+    List<Order> orders = orderClient.getUsersDetails(Integer.valueOf(dto.getUserId()));
+    List<com.pm.grpc.users.Order> orders2 = getOrders(orders);
+    channel.awaitTermination(3, TimeUnit.SECONDS);
+
+    UserResponse userResponse =
+        UserResponse.newBuilder()
+            .setUserId(dto.getUserId())
+            .setName(dto.getName())
+            .setUsername(dto.getUsername())
+            .setAge(dto.getAge())
+            .addAllOrders(orders2)
+            .build();
 
         responseObserver.onNext(userResponse);
         responseObserver.onCompleted();
+  }
 
+  private List<com.pm.grpc.users.Order> getOrders(List<Order> orders) {
+    List<com.pm.grpc.users.Order> orders2 =
+        orders.stream()
+            .map(
+                orderDto -> {
+                  return com.pm.grpc.users.Order.newBuilder()
+                      .setOrderId(orderDto.getOrderId())
+                      .setUserId(orderDto.getUserId())
+                      .setProductName(orderDto.getProductName())
+                      .setDescription(orderDto.getDescription())
+                      .setTotalPrice(orderDto.getTotalPrice())
+                      .build();
+                })
+            .collect(Collectors.toList());
+    return orders2;
     }
 }
